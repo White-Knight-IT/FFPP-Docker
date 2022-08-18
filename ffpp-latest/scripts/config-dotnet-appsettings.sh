4b65dcb5a08dec68b59aa0a1da22ab47ecb59ceb
knightian <ian@whiteknightit.com.au>
2022-08-19 09:00:20 +1000

    More settings

--- ffpp-latest/scripts/config-dotnet-appsettings.sh
+++ ffpp-latest/scripts/config-dotnet-appsettings.sh
@@ -5,6 +5,7 @@ then
   export SHOW_DEV_ENDPOINTS=false
 else
   export SHOW_DEV_ENDPOINTS=true
+fi
 
 envsubst '${FFPP_DOMAIN} ${MARIADB_USER} ${MARIADB_PASSWORD} ${MARIADB_SERVER} ${MARIADB_PORT} ${SHOW_DEV_ENDPOINTS} ${DNS_PROVIDER}' < /root/config/templates/appsettings.template > /root/ffpp/built/appsettings.json
 
