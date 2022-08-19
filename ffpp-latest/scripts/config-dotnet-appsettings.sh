94c5a612ff032abfc400136ba3768823ed5e690c
knightian <ian@whiteknightit.com.au>
2022-08-19 12:57:30 +1000

    More settings

--- ffpp-latest/scripts/config-dotnet-appsettings.sh
+++ ffpp-latest/scripts/config-dotnet-appsettings.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-echo $SHOW_DEV_ENDPOINTS
-echo $SHOW_DEV_ENDPOINTS
-echo $SHOW_DEV_ENDPOINTS
-echo $SHOW_DEV_ENDPOINTS
-echo $SHOW_DEV_ENDPOINTS
-
-if [ "$SHOW_DEV_ENDPOINTS" = "'false'" ]
-then
-  export SHOW_DEV_ENDPOINTS=false
-else
-  export SHOW_DEV_ENDPOINTS=true
-fi
-
 envsubst '${FFPP_DOMAIN} ${MARIADB_USER} ${MARIADB_PASSWORD} ${MARIADB_SERVER} ${MARIADB_PORT} ${SHOW_DEV_ENDPOINTS} ${DNS_PROVIDER}' < /root/config/templates/appsettings.template > /root/ffpp/built/appsettings.json
 
 cp /root/ffpp/built/appsettings.json /root/ffpp/src/FFPP/appsettings.json
--- linux/recovery_scripts/delete-all-docker-images.sh
+++ linux/recovery_scripts/delete-all-docker-images.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Stop all running containers
+sudo docker stop $(sudo docker ps -a -q)
+
+# Delete all images (which also deletes containers)
 sudo docker image ls -q | xargs -I {} sudo docker image rm -f {}
