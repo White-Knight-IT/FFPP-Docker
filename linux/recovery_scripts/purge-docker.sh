0a0ed9097d9e8a5757eebe9743af8e96f15fc1db
knightian <ian@whiteknightit.com.au>
2022-08-19 13:26:22 +1000

    More settings

--- linux/recovery_scripts/purge-docker.sh
+++ linux/recovery_scripts/purge-docker.sh
@@ -3,5 +3,8 @@
 # Stop all running containers
 sudo docker stop $(sudo docker ps -a -q)
 
-# Delete all images (which also deletes containers)
+# Delete all containers
+sudo docker rm $(sudo docker ps -a -q)
+
+# Delete all images
 sudo docker image ls -q | xargs -I {} sudo docker image rm -f {}
