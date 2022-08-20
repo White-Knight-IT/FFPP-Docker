e706cc938f41542497089346802e13d65e6e0528
knightian <ian@whiteknightit.com.au>
2022-08-20 17:20:51 +1000

    add scripts

--- linux/scripts/make-swapfile.sh
+++ linux/scripts/make-swapfile.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+echo "Checking if swapfile already exists: "
+echo " "
+sudo swapon --show
+echo " "
+read -p 'Do you see a swapfile in the above output? [y/N]' inputVar
+if ($inputVar == 'y' || $inputVar == 'Y')
+then
+  echo "Cancelling swapfile creation"
+  exit 0
+else
+  echo "Proceeding to create swapfile"
+fi
 sudo fallocate -l 2G /swapfile
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+echo " "
 echo "Swapfile created and added to /etc/fstab"
