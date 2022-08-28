b4afc9f5596d2701af3d2a89bd3da98d63baf230
knightian <ian@whiteknightit.com.au>
2022-08-29 00:08:38 +1000

    add scripts

--- linux/recovery_scripts/purge.sh
+++ linux/recovery_scripts/purge.sh
@@ -6,4 +6,4 @@ sudo rm -rf ~/FFPP-Docker/shared_persistent_volume/mariadb_data/*
 
 sudo rm ~/FFPP-Docker/shared_persistent_volume/api.zeroconf.aes
 
-sudo sh ~/FFPP-Docker/linux/recovery_scripts/purge_docker.sh
+sh ~/FFPP-Docker/linux/recovery_scripts/purge_docker.sh
